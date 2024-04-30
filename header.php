e4eeaa3eb06d2b93c163071c78d3899e817ce2c8
Binil <bnilbajracharya@gmail.com>
2024-05-01 00:01:26 +0545

    first commit

---
+++ header.php
@@ -0,0 +1,79 @@
+<!-- Created by Binil-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Eduvi</title>
+    <!------------------------ Meta Starts ---------------------->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width">
+    <link rel="shortcut icon" type="image/x-icon" href="img/site_logo.png">
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:image:alt" content="">
+    <meta property="og:description" content="">
+    <!------------------------- Meta Ends ----------------------->
+    <!----------------------- Fonts Starts ---------------------->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <!------------------------ Fonts Ends ----------------------->
+
+    <!-------------------- Bootstrap Starts -------------------->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-------------------- Components Starts -------------------->
+    <link href="css/bowercomponent/all.min.css">
+    <link href="css/bowercomponent/slick.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!--------------------- Components Ends --------------------->
+    <!-------------------- Site Style Starts -------------------->
+    <link href="css/global.css" rel="stylesheet">
+    <link href="css/thestyles.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <!--------------------- Site Style Ends --------------------->
+</head>
+
+<body>
+    <header>
+        <section id="header">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-9 col-lg-2"><a class="navbar-brand" href="index.php"><img src="./img/logo/Logo.jpg"></a></div>
+
+                    <div class="col-3 col-lg-6">
+                        <nav class="navbar navbar-expand-lg navbar-light text-white">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item"><a class="nav-link" href="#">Shop</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">For Kindergarten</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">For High School</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">For College</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Courses</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+
+                    <div class="col-lg-4 d-none d-xxl-block">
+                        <ul class="d-flex justify-content-end">
+                            <li class="nav-item"><a class="nav-link" href="#"> Cart (0) <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M23.75 8.75H20V7.5C20 6.17392 19.4732 4.90215 18.5355 3.96447C17.5979 3.02678 16.3261 2.5 15 2.5C13.6739 2.5 12.4021 3.02678 11.4645 3.96447C10.5268 4.90215 10 6.17392 10 7.5V8.75H6.25C5.91848 8.75 5.60054 8.8817 5.36612 9.11612C5.1317 9.35054 5 9.66848 5 10V23.75C5 24.7446 5.39509 25.6984 6.09835 26.4017C6.80161 27.1049 7.75544 27.5 8.75 27.5H21.25C22.2446 27.5 23.1984 27.1049 23.9017 26.4017C24.6049 25.6984 25 24.7446 25 23.75V10C25 9.66848 24.8683 9.35054 24.6339 9.11612C24.3995 8.8817 24.0815 8.75 23.75 8.75ZM12.5 7.5C12.5 6.83696 12.7634 6.20107 13.2322 5.73223C13.7011 5.26339 14.337 5 15 5C15.663 5 16.2989 5.26339 16.7678 5.73223C17.2366 6.20107 17.5 6.83696 17.5 7.5V8.75H12.5V7.5ZM22.5 23.75C22.5 24.0815 22.3683 24.3995 22.1339 24.6339C21.8995 24.8683 21.5815 25 21.25 25H8.75C8.41848 25 8.10054 24.8683 7.86612 24.6339C7.6317 24.3995 7.5 24.0815 7.5 23.75V11.25H10V12.5C10 12.8315 10.1317 13.1495 10.3661 13.3839C10.6005 13.6183 10.9185 13.75 11.25 13.75C11.5815 13.75 11.8995 13.6183 12.1339 13.3839C12.3683 13.1495 12.5 12.8315 12.5 12.5V11.25H17.5V12.5C17.5 12.8315 17.6317 13.1495 17.8661 13.3839C18.1005 13.6183 18.4185 13.75 18.75 13.75C19.0815 13.75 19.3995 13.6183 19.6339 13.3839C19.8683 13.1495 20 12.8315 20 12.5V11.25H22.5V23.75Z" fill="#FF6652" />
+                                    </svg>
+                                </a>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="#"> My Account <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M14.9999 2.5C12.5755 2.50461 10.2048 3.21414 8.17648 4.54217C6.14816 5.87021 4.54981 7.75943 3.57612 9.97971C2.60242 12.2 2.29541 14.6555 2.69247 17.0472C3.08953 19.4389 4.17354 21.6635 5.81244 23.45C6.98297 24.7188 8.40362 25.7314 9.98486 26.424C11.5661 27.1166 13.2737 27.4742 14.9999 27.4742C16.7262 27.4742 18.4338 27.1166 20.015 26.424C21.5963 25.7314 23.0169 24.7188 24.1874 23.45C25.8263 21.6635 26.9103 19.4389 27.3074 17.0472C27.7045 14.6555 27.3975 12.2 26.4238 9.97971C25.4501 7.75943 23.8517 5.87021 21.8234 4.54217C19.7951 3.21414 17.4243 2.50461 14.9999 2.5ZM14.9999 25C12.4105 24.9961 9.92358 23.9878 8.06244 22.1875C8.62749 20.8119 9.58873 19.6354 10.824 18.8074C12.0593 17.9793 13.5128 17.5373 14.9999 17.5373C16.4871 17.5373 17.9406 17.9793 19.1759 18.8074C20.4111 19.6354 21.3724 20.8119 21.9374 22.1875C20.0763 23.9878 17.5894 24.9961 14.9999 25ZM12.4999 12.5C12.4999 12.0055 12.6466 11.5222 12.9213 11.1111C13.196 10.7 13.5864 10.3795 14.0432 10.1903C14.5 10.0011 15.0027 9.95157 15.4877 10.048C15.9726 10.1445 16.4181 10.3826 16.7677 10.7322C17.1173 11.0819 17.3554 11.5273 17.4519 12.0123C17.5484 12.4972 17.4989 12.9999 17.3096 13.4567C17.1204 13.9135 16.8 14.304 16.3889 14.5787C15.9777 14.8534 15.4944 15 14.9999 15C14.3369 15 13.701 14.7366 13.2322 14.2678C12.7633 13.7989 12.4999 13.163 12.4999 12.5ZM23.6374 20C22.5206 18.0897 20.8017 16.6038 18.7499 15.775C19.3864 15.0533 19.8011 14.1633 19.9443 13.2118C20.0875 12.2603 19.953 11.2876 19.5571 10.4106C19.1612 9.5336 18.5207 8.78945 17.7123 8.26746C16.904 7.74547 15.9622 7.46781 14.9999 7.46781C14.0377 7.46781 13.0959 7.74547 12.2876 8.26746C11.4792 8.78945 10.8387 9.5336 10.4428 10.4106C10.0468 11.2876 9.91242 12.2603 10.0556 13.2118C10.1988 14.1633 10.6135 15.0533 11.2499 15.775C9.19819 16.6038 7.47928 18.0897 6.36244 20C5.47237 18.4839 5.00209 16.7581 4.99994 15C4.99994 12.3478 6.05351 9.8043 7.92887 7.92893C9.80424 6.05357 12.3478 5 14.9999 5C17.6521 5 20.1956 6.05357 22.071 7.92893C23.9464 9.8043 24.9999 12.3478 24.9999 15C24.9978 16.7581 24.5275 18.4839 23.6374 20Z" fill="#9C4DF4" />
+                                    </svg>
+                                </a></li>
+                        </ul>
+                    </div>
+        </section>
+    </header>
---
+++ thescripts.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Sushant Gauchan
+ */
+
+var winWidth = $(window).width();
+
+$(document).ready(function () {
+  sliderInit();
+});
+
+/*------------------------------- Functions Starts -------------------------------*/
+function sliderInit() {
+  $(".testimonial-slider").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    speed: 500,
+    // fade: true,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
+  });
+}
+
+
+
+
+//Sticky Header
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+      $("header").addClass("active");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $("header").removeClass("active");
+    }
+  });
+});
+/*-------------------------------- Functions Ends --------------------------------*/
